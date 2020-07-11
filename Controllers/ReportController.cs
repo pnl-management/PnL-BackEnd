5df361392ef38c6521c91b63cb8ebe08bdb7449c
Thien Phuc <ttran.phuc99@gmail.com>
2020-07-11 12:17:24 +0700

    mov data to gg sheet

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using PnLReporter.EnumInfo;
+using PnLReporter.Models;
+using PnLReporter.Service;
 
 namespace PnLReporter.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ReportController : ControllerBase
     {
         private static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
@@ -20,6 +28,53 @@ namespace PnLReporter.Controllers
         private static readonly string SpreadsheetId = "1CreuKDEYZEWqGChVJO31S8PlhbtdqhMiJBHvUhEZJaY";
         private static readonly string SheetName = "Sheet1";
         private static SheetsService service;
+
+        private readonly PLSystemContext _context;
+        private readonly IReportService _service;
+
+        public ReportController(PLSystemContext context, IDistributedCache cache)
+        {
+            _context = context;
+            _service = new ReportService(context, cache);
+        }
+
+        [HttpGet("/api/reports")]
+        [Authorize(Roles = ParticipantsRoleConst.ACCOUNTANT + "," + ParticipantsRoleConst.INVESTOR)]
+        public ActionResult GetReport()
+        {
+            var user = this.GetCurrentUserInfo();
+            int storeSize;
+            var listReport = _service.ListDataToGgSheet(user.Brand.Id, out storeSize);
+
+            GoogleCredential credential;
+
+            using (var stream = new FileStream("google-sheet-api-7f7e9f359cf6.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+                service = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = AppName,
+                });
+            }
+
+            char endColumn = (char) ((storeSize + 65) > 90 ? 90 : (storeSize + 65));
+
+            for (int i = 0; i < listReport.Count(); i++)
+            {
+                String rangeVal = "A" + (i+1) + ":" + endColumn + (i+1);
+
+                var range = $"{SheetName}!{rangeVal}";
+                var valueRange = new ValueRange();
+                valueRange.Values = new List<IList<object>> { listReport[i] };
+
+                var request = service.Spreadsheets.Values.Update(valueRange, SpreadsheetId, range);
+                request.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
+                request.Execute();
+            }
+
+            return Ok();
+        }
 
         [HttpGet("/gg-sheet")]
         public ActionResult GetList()
@@ -120,10 +175,10 @@ namespace PnLReporter.Controllers
 
         private object UpdateEntries()
         {
-            var range = $"{SheetName}!A7:C7";
+            var range = $"{SheetName}!A8:C8";
             var valueRange = new ValueRange();
 
-            var objLst = new List<object>() { "Quý 7 / 2020", "9383", "829" };
+            var objLst = new List<object>() { "Quý 7sss / 2020", "9383", "829" };
             valueRange.Values = new List<IList<object>> { objLst };
 
             var request = service.Spreadsheets.Values.Update(valueRange, SpreadsheetId, range);
@@ -139,6 +194,20 @@ namespace PnLReporter.Controllers
 
             var request = service.Spreadsheets.Values.Clear(valueRange, SpreadsheetId, range);
             return request.Execute();
+        }
+
+        private UserModel GetCurrentUserInfo()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            string role = identity.FindFirst(ClaimTypes.Role).Value;
+            string participantIdVal = identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            long participantsId;
+
+            long.TryParse(participantIdVal, out participantsId);
+            IParticipantService participantService = new ParticipantService(_context);
+
+            return participantService.FindByUserId(participantsId);
         }
     }
 }
---
+++ ViewModels/ReportVModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PnLReporter.ViewModels
+{
+    public class ReportVModel
+    {
+        public String Id {
+            get
+            {
+                if (Period != null && Store != null)
+                {
+                    return Period.Id + " - " + Store.Id;
+                }
+                return null;
+            }
+        }
+        public AccountingPeriodVModel Period { get; set; }
+        public StoreVModel Store { get; set; }
+        public IEnumerable<TransactionVModel> ListTransactions { get; set; }
+    }
+}
