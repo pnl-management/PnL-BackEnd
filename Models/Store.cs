6d2c121b6557d96be60c730d77a699a0841508e1
Thien Phuc <ttran.phuc99@gmail.com>
2020-07-21 13:34:37 +0700

    add json ignore

--- Models/Store.cs
+++ Models/Store.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace PnLReporter.Models
@@ -19,9 +20,13 @@ namespace PnLReporter.Models
         public bool? Status { get; set; }
         public int? BrandId { get; set; }
 
+        [JsonIgnore]
         public virtual Brand Brand { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Receipt> Receipt { get; set; }
+        [JsonIgnore]
         public virtual ICollection<StoreParticipantsDetail> StoreParticipantsDetail { get; set; }
+        [JsonIgnore]
         public virtual ICollection<Transaction> Transaction { get; set; }
     }
 }
